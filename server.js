c5b5613ae20a1bdacb6ae620a5d4e1b982b73459
Ying Wang <ext-ying.wang@here.com>
2018-04-16 23:40:23 +0200

    Fix linter issues

--- server.js
+++ server.js
@@ -1,3 +1,4 @@
+'use strict';
 let express = require('express');
 let bodyParser = require('body-parser');
 let store = require('./store');
@@ -5,37 +6,37 @@ let app = express();
 
 let jsonParser = bodyParser.json();
 
-app.get('/tasks', function (req, res) {
-  console.log("Got a GET request for the tasks page");
+app.get('/tasks', function(req, res) {
+  console.log('Got a GET request for the tasks page');
   res.send(JSON.stringify(store.getAll()));
-})
+});
 
-app.post('/tasks', jsonParser, function (req, res) {
-  console.log("Got a POST request for the tasks page");
+app.post('/tasks', jsonParser, function(req, res) {
+  console.log('Got a POST request for the tasks page');
   let task_id = store.createTaskAndReturnID(req.body);
   res.send(task_id);
-})
+});
 
-app.get('/tasks/:taskID', function (req, res) {
-  console.log("Got a GET request for a task with taskID");
+app.get('/tasks/:taskID', function(req, res) {
+  console.log('Got a GET request for a task with taskID');
   res.send(JSON.stringify(store.getTaskByID(req.params.taskID)));
-})
+});
 
-app.delete('/tasks/:taskID', function (req, res) {
-  console.log("Got a DELETE request for tasks page with taskID");
+app.delete('/tasks/:taskID', function(req, res) {
+  console.log('Got a DELETE request for tasks page with taskID');
   store.deleteTaskByID(req.params.taskID);
   res.send(`deleted task with ID ${req.params.taskID}`);
-})
+});
 
-app.patch('/tasks/:taskID', jsonParser, function (req, res) {
-  console.log("Got a PATCH request for tasks page with taskID");
+app.patch('/tasks/:taskID', jsonParser, function(req, res) {
+  console.log('Got a PATCH request for tasks page with taskID');
   store.modifyTaskByID(req.params.taskID, req.body);
   res.send(`modify task with ID ${req.params.taskID}`);
-})
+});
 
-let server = app.listen(8081, function () {
-  let host = server.address().address
-  let port = server.address().port
-   
-  console.log(`Todo app listening at http://${host}:${port}`)
-})
+let server = app.listen(8081, function() {
+  let host = server.address().address;
+  let port = server.address().port;
+
+  console.log(`Todo app listening at http://${host}:${port}`);
+});
--- store.js
+++ store.js
@@ -1,26 +1,27 @@
+'use strict';
 let uuid = require('uuid/v4');
 
-let tasks = {}
+let tasks = {};
 module.exports = {
-  getAll: function () {
-    let tasks_array = []
+  getAll: function() {
+    let tasks_array = [];
     Object.keys(tasks).forEach(function(taskID) {
-      tasks_array.push({id: taskID, task: tasks[taskID]})
+      tasks_array.push({id: taskID, task: tasks[taskID]});
     });
-    return tasks_array
+    return tasks_array;
   },
-  createTaskAndReturnID: function (task) {
-    let task_id = uuid()
-    tasks[task_id] = task
-    return task_id
+  createTaskAndReturnID: function(task) {
+    let task_id = uuid();
+    tasks[task_id] = task;
+    return task_id;
   },
-  getTaskByID: function (taskID) {
-    return tasks[taskID]
+  getTaskByID: function(taskID) {
+    return tasks[taskID];
   },
-  deleteTaskByID: function (taskID) {
-    delete tasks[taskID]
+  deleteTaskByID: function(taskID) {
+    delete tasks[taskID];
   },
-  modifyTaskByID: function (taskID, new_content) {
-    tasks[taskID] = Object.assign(tasks[taskID], new_content)
-  }
-}
+  modifyTaskByID: function(taskID, new_content) {
+    tasks[taskID] = Object.assign(tasks[taskID], new_content);
+  },
+};
