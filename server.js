50f26fdabc2a8a46076d37d626a54faece94f405
Ying Wang <ext-ying.wang@here.com>
2018-04-17 19:49:35 +0200

    add labels endpoint

--- server.js
+++ server.js
@@ -6,9 +6,10 @@ let app = express();
 
 let jsonParser = bodyParser.json();
 
+// api endpoint tasks
 app.get('/tasks', function(req, res) {
   console.log('Got a GET request for the tasks page');
-  res.send(JSON.stringify(store.getAll()));
+  res.send(JSON.stringify(store.getAllTasks()));
 });
 
 app.post('/tasks', jsonParser, function(req, res) {
@@ -32,6 +33,35 @@ app.patch('/tasks/:taskID', jsonParser, function(req, res) {
   console.log('Got a PATCH request for tasks page with taskID');
   store.modifyTaskByID(req.params.taskID, req.body);
   res.send(`modify task with ID ${req.params.taskID}`);
+});
+
+// api endpoint labels
+app.get('/labels', function(req, res) {
+  console.log('Got a GET request for the labels');
+  res.send(JSON.stringify(store.getAllLabels()));
+});
+
+app.post('/labels', jsonParser, function(req, res) {
+  console.log('Got a POST request for the labels');
+  let label_id = store.createLabelAndReturnID(req.body);
+  res.send(label_id);
+});
+
+app.get('/labels/:labelID', function(req, res) {
+  console.log('Got a GET request for a label with labelID');
+  res.send(JSON.stringify(store.getLabelByID(req.params.labelID)));
+});
+
+app.delete('/labels/:labelID', function(req, res) {
+  console.log('Got a DELETE request for labels with labelID');
+  store.deleteLabelByID(req.params.labelID);
+  res.send(`deleted label with ID ${req.params.labelID}`);
+});
+
+app.patch('/labels/:labelID', jsonParser, function(req, res) {
+  console.log('Got a PATCH request for labels with labelID');
+  store.modifyLabelByID(req.params.labelID, req.body);
+  res.send(`modify label with ID ${req.params.labelID}`);
 });
 
 let server = app.listen(8081, function() {
--- store.js
+++ store.js
@@ -2,10 +2,11 @@
 let uuid = require('uuid/v4');
 
 let tasks = {};
+let labels = {};
 module.exports = {
-  getAll: function() {
+  getAllTasks: function() {
     let tasks_array = [];
-    Object.keys(tasks).forEach(function(taskID) {
+    Object.keys(tasks).forEach(taskID => {
       tasks_array.push({id: taskID, task: tasks[taskID]});
     });
     return tasks_array;
@@ -23,5 +24,26 @@ module.exports = {
   },
   modifyTaskByID: function(taskID, new_content) {
     tasks[taskID] = Object.assign(tasks[taskID], new_content);
+  },
+  getAllLabels: function() {
+    let labels_array = [];
+    Object.keys(labels).forEach(labelID => {
+      labels_array.push({id: labelID, label: labels[labelID]});
+    });
+    return labels_array;
+  },
+  createLabelAndReturnID: function(label) {
+    let label_id = uuid();
+    labels[label_id] = label;
+    return label_id;
+  },
+  getLabelByID: function(labelID) {
+    return labels[labelID];
+  },
+  deleteLabelByID: function(labelID) {
+    delete labels[labelID];
+  },
+  modifyLabelByID: function(labelID, new_content) {
+    tasks[labelID] = Object.assign(labels[labelID], new_content);
   },
 };
